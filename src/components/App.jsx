e5b00700c03e6f49045f41187d0bcd52f21fbee6
NatalaBoiko <natalabojko@gmail.com>
2022-07-18 23:14:15 +0300

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,8 @@ export class App extends Component {
     };
     this.state.contacts.some(
       i =>
-        i.name.toLowerCase() === contact.name.toLowerCase() &&
+        (i.name.toLowerCase() === contact.name.toLowerCase() &&
+          i.number === contact.number) ||
         i.number === contact.number
     )
       ? alert(`${name} is already in contacts`)
